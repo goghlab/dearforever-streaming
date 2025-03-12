98368776c003d37c2d3f696abf8abfeaf71a32be
dom <dom851013@gmail.com>
2025-03-13 01:41:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/akool-streaming-avatar-react-demo' : '/streaming/avatar',
+  base: process.env.NODE_ENV === 'production' ? '/streaming/avatar/' : '/', // Adjusted base path
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
   },
-})
+});
